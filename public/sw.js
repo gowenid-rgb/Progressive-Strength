628a985bb7134442dd2edf75ffa54f6d0035d91e
Greg Owen <gowen@exemplis.com>
2026-09-09 15:57:33 -0700

    feat: turn app into a Progressive Web App (PWA) with manifest, sw, and icons

---
+++ public/sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'prog-strength-v1';
+const ASSETS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon-192.jpg',
+  '/icon-512.jpg'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
